9a4f684c9a9afbe5814fd6b87d0ee936214ccac5
webtonics <camplodgeinc@gmail.com>
2024-01-01 23:42:07 -0800

    responsive cont 2

--- lib/pages/calculators/adrevenue_calculator.dart
+++ lib/pages/calculators/adrevenue_calculator.dart
@@ -11,14 +11,26 @@ class _AdRevenueCalcViewState extends State<AdRevenueCalcView> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      backgroundColor: const Color.fromARGB(255, 230, 225, 225),
       appBar: AppBar(
         backgroundColor: Colors.amber,
         title: const Text("Ad Revenue Calculator", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
       ),
-      body: const Column(
-        children: [
-          Center(child: Text("Under Construction"))
-        ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+          child: Container(
+             decoration:  BoxDecoration( color: Colors.white, borderRadius: BorderRadius.circular(23)),
+               alignment: Alignment.center,
+             
+             width: 500,
+            child: const Column(
+              children: [
+                Center(child: Text("Under Construction"))
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/pages/calculators/calories_calculator.dart
+++ lib/pages/calculators/calories_calculator.dart
@@ -11,15 +11,26 @@ class _CalorieCalcViewState extends State<CalorieCalcView> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      backgroundColor: const Color.fromARGB(255, 230, 225, 225),
       appBar: AppBar(
         backgroundColor: Colors.amber,
         title: const Text("Calorie Calculator", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
       ),
-      body: const Column(
-        children: [
-          Center(child: Text("Under Construction"))
-        ],
-      ),
-    );
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+          child: Container(
+              decoration:  BoxDecoration( color: Colors.white, borderRadius: BorderRadius.circular(23)),
+               alignment: Alignment.center,
+             
+             width: 500,
+            child: const Column(
+           children: [
+             Center(child: Text("Under Construction"))
+           ],
+            ),
+          ),
+        ),
+      ));
   }
 }
